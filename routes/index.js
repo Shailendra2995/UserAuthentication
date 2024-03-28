51e17dc2d7734723c5f81bb1d2001889b911c989
Shailendra2995 <skarki5277@conestogac.on.ca>
2024-03-28 12:56:26 -0400

    small change

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.get('/login', Controller.login_get)
 router.post('/login', Controller.login_post)
-router.get('/home', Controller.home_get)
+router.get('/', Controller.home_get)
 router.get('/dashboard', isValid, Controller.dashboard_get)
 router.post('/logout', Controller.logout_post)
 router.get('/signup', Controller.signup_get)
